728e2d40982ece9bd225c714147f06510812c431
Shawn Meas <shawn@smeas.info>
2022-08-13 08:10:22 -0400

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
+const ftoc = function(tempIn) {
+  let tempOut;
+  
+  tempOut = (tempIn - 32) * (5/9);
+  tempOut = Math.round(tempOut * 10) / 10;
 
+  return tempOut;
 };
 
-const ctof = function() {
+const ctof = function(tempIn) {
+  let tempOut;
 
+  tempOut = (tempIn * (9/5) + 32);
+  tempOut = Math.round(tempOut * 10) / 10;
+
+  return tempOut;
 };
 
 // Do not edit below this line
