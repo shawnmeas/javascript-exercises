ee35302081a0e998632301e911ff80d75b2903b1
Shawn Meas <shawn@smeas.info>
2022-08-13 07:53:01 -0400

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let arrayIn = args[0];
+    let arrayOut = [];
 
+    arrayIn.forEach((i) =>
+    {
+        if (!args.includes(i))
+        {
+            arrayOut.push(i);
+        }
+    });
+    
+    return arrayOut;
 };
 
 // Do not edit below this line
