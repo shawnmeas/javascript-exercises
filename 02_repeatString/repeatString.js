d3084860cbd5ed0dca4c943230c3e79c13a6c733
Shawn Meas <shawn@smeas.info>
2022-08-13 07:33:16 -0400

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringIn, num) {
+    let stringOut = '';
+    for (let i = 0; i < num; i++)
+    {
+        stringOut += stringIn;
+    }
+    return stringOut;
 };
 
 // Do not edit below this line
