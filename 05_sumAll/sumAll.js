f2140f862f19b3cad157a988621d98d3170097a2
Shawn Meas <shawn@smeas.info>
2022-08-13 07:59:17 -0400

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if(num1 < 0 || num2 < 0) return "ERROR";
+    let numOut = 0;
 
+    firstNum = Math.min(num1, num2);
+    secondNum = Math.max(num1, num2);
+    for(let i = firstNum; i <= secondNum; i++)
+    {
+        numOut += i;
+    }
+
+    return numOut;
 };
 
 // Do not edit below this line
