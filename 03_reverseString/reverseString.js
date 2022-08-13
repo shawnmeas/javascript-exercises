62270254d3ab9b5bbbed50adb7c71106b6d75ff8
Shawn Meas <shawn@smeas.info>
2022-08-13 07:42:13 -0400

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(stringIn) {
+    let stringOut = '';
+    let stringLen = 0;
+    stringLen = stringIn.length;
+    let newLength = stringLen - 1;
 
+    for (let i = 0; i < stringLen; i++)
+    {
+        stringOut += stringIn.substr(newLength, 1);
+        newLength--;
+    }
+
+    return stringOut;
 };
 
 // Do not edit below this line
