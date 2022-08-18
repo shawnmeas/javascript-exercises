9cd1be32908f63ddfe0a77b0c38cc6b11d647e45
Shawn Meas <shawn@smeas.info>
2022-08-18 04:31:07 -0400

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if(a <= 0)
+        return 'OOPS';
+
+    let result = 0;
+    let fiboNum = 1;
+    let old;
+    for(let i = 1; i < a; i++)
+    {
+        old = fiboNum;
+        fiboNum = result + fiboNum;
+        result = old;
+    }
+    return fiboNum;
 
 };
 
