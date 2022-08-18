90c880448601cdc8053dd1f4d9194a0b2883a055
Shawn Meas <shawn@smeas.info>
2022-08-18 04:45:57 -0400

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(a) {
+    let oldest = 0;
+    let oldestIndex, age;
+    for(let i = 0; i < a.length; i++)
+    {
+        if(a[i].yearOfDeath)
+            age = a[i].yearOfDeath - a[i].yearOfBirth;
+        else
+            age = 2022 - a.[i].yearOfBirth;
+        if(age > oldest)
+        {
+            oldest = age;
+            oldestIndex = i;
+        }
+    }
 
+    return a[oldestIndex];
 };
 
 // Do not edit below this line
