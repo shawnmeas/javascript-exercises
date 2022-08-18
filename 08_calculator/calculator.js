0ed5071b6e82299b16a91c8b08b3c8ae1634ee63
Shawn Meas <shawn@smeas.info>
2022-08-18 04:12:51 -0400

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let result = 0;
+  for(let i = 0; i < a.length; i++)
+    result = result + a[i];
+	return result;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let result = a[0];
+  for(let i = 1; i < a.length; i++)
+    result = result * a[i];
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a == 0)
+    return 1;
+  let result = 1;
+  for(let i = a; i > 0; i--)
+    result = result * i;
+	return result;
 };
 
 // Do not edit below this line
