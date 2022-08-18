5c0a58e3ef7c1601d520df1f1f12c0d90e61a8bb
Shawn Meas <shawn@smeas.info>
2022-08-18 04:21:33 -0400

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let output = '';
+    input = input.toLowerCase();
+    input = input.replace(/[^a-z0-9]/gi, '');
+    for(let i = input.length; i >= 0; i--)
+        output = output + input.slice(i-1, i);
 
+    if(output == input)
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
